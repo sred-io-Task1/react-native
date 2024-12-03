10678c4a0b0a3c6b12d20813785b529b177e658e
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-12-03 11:46:47 +0100

    move init of variable to if scope

--- packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputComponentView.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputComponentView.mm
@@ -269,12 +269,12 @@ static NSSet<NSNumber *> *returnKeyTypesSet;
         _backedTextInputView.defaultTextAttributes[RCTAttributedStringEventEmitterKey];
 #endif
 
-      NSMutableParagraphStyle *paragraphStyle = defaultAttributes[NSParagraphStyleAttributeName];
-
       if (!newTextInputProps.multiline) {
+          NSMutableParagraphStyle *paragraphStyle = defaultAttributes[NSParagraphStyleAttributeName];
           paragraphStyle.lineBreakMode = NSLineBreakByTruncatingTail;
           defaultAttributes[NSParagraphStyleAttributeName] = paragraphStyle;
       }
+
     _backedTextInputView.defaultTextAttributes = defaultAttributes;
   }
 
